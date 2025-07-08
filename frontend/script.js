f7a5cdc42dadd5027041a14522711e6e2f7efbbf
saiveerendra <113809676+saiveerendra@users.noreply.github.com>
2025-07-08 14:48:09 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://fealtyx-backend.onrender.com";
 
 const form = document.getElementById("studentForm");
 const studentList = document.getElementById("studentList");
