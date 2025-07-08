b12e416c2441163d00664acbb4a09629613ae03b
saiveerendra <113809676+saiveerendra@users.noreply.github.com>
2025-07-08 15:33:57 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://fealtyx-assignment-1.onrender.com/";
 
 const form = document.getElementById("studentForm");
 const studentList = document.getElementById("studentList");
